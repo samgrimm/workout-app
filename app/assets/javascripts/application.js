776992c6f0cc1b33367ffb5a2f87f219874fddf0
samgrimm <samantha.grimm@gmail.com>
2017-02-01 13:00:17 -0600

    add jquery datepicker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,9 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery-ui/datepicker
 //= require jquery_ujs
 //= require tether
 //= require bootstrap-sprockets
-//= require turbolinks
+// require turbolinks
 //= require_tree .
--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -3,5 +3,5 @@ class Exercise < ApplicationRecord
 
   validates :workout_date, presence: true
   validates :workout, presence: true
-  validates :duration_in_min, numericality: true
+  validates :duration_in_min, numericality: {greater_than: 0.0}
 end
